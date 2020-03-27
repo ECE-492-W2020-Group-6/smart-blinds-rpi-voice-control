313c548294883f3b21776ca65c753d08051eb3cf
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-03-27 13:30:47 -0600

    Remove stop words from voice parser

--- rpi_voice_control/voice_parser.py
+++ rpi_voice_control/voice_parser.py
@@ -1,6 +1,15 @@
+"""
+Date: Mar 27, 2020
+Author: Ishaat Chowdhury
+Contents: Contains class that parses voice input
+"""
+
 from text2digits.text2digits import Text2Digits
 import re
+from nltk import download
 from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+download("stopwords")
 
 class VoiceParser():
     # Class variables
@@ -12,14 +21,25 @@ class VoiceParser():
     @classmethod
     def parse(cls, line):
         # Replace substrings with number as word to digits
-        # Ex. "one hundred" -> "100"
+        # Ex. "move blinds to minus fifty-seven point one one three" 
+        #       -> "move blinds to minus 57 point 113 percent"
         result = cls.TEXT2D.convert(line)
+
         # Replace substrings with integers around word "point" with decimal representation
-        # Ex. "five point 7" -> "5.7" 
+        # Ex. "move blinds to minus 57 point 113 percent" 
+        #       -> "move blinds to minus 57.113 percent"
         result = cls.DECIMAL_PATTERN.sub(r"\g<1>.\g<2>", result)
+
         # Replace substrings with "minus"/"negative" preceding integer/decimal with -integer/decimal
-        # Ex. "minus 13.159" -> "-13.159"
+        # Ex. "move blinds to minus 57.113 percent" -> "move blinds to -57.113 percent"
         result = cls.NEGATIVE_PATTERN.sub(r"-\g<1>", result)
+
         # Stem words
+        # Ex. "move blinds to -57.113 percent" -> "move blind to -57.113 percent"
         result = " ".join(map(cls.STEMMER.stem, result.split()))
+
+        # Remove stop words (ex. to, is, not)
+        # Ex. "move blind to -57.113 percent" -> "move blind -57.113 percent"
+        result = " ".join(filter(lambda w: w not in stopwords.words(), result.split()))
+
         return result
--- tests/test_voice_parser.py
+++ tests/test_voice_parser.py
@@ -1,3 +1,9 @@
+"""
+Date: Mar 27, 2020
+Author: Ishaat Chowdhury
+Contents: Contains unit tests for voice parser
+"""
+
 import pytest
 from rpi_voice_control.voice_parser import VoiceParser
 
@@ -6,27 +12,27 @@ from rpi_voice_control.voice_parser import VoiceParser
     ("", ""),
     ("hello world", "hello world"),
     # Positive Integers
-    ("move blinds to ten percent", "move blind to 10 percent"),
-    ("move blinds to ninety five percent", "move blind to 95 percent"),
-    ("move blinds to fourteen percent", "move blind to 14 percent"),
+    ("move blinds to ten percent", "move blind 10 percent"),
+    ("move blinds to ninety five percent", "move blind 95 percent"),
+    ("move blinds to fourteen percent", "move blind 14 percent"),
     # Positive Decimals
-    ("move blinds to five point three percent", "move blind to 5.3 percent"),
-    ("move blinds to eighty-seven point seventeen percent", "move blind to 87.17 percent"),
-    ("move blinds to seventy one point three five percent", "move blind to 71.35 percent"),
+    ("move blinds to five point three percent", "move blind 5.3 percent"),
+    ("move blinds to eighty-seven point seventeen percent", "move blind 87.17 percent"),
+    ("move blinds to seventy one point three five percent", "move blind 71.35 percent"),
     # Negative Integers
-    ("move blinds to minus twelve percent", "move blind to -12 percent"),
-    ("move blinds to minus sixty three percent", "move blind to -63 percent"),
-    ("move blinds to minus fifteen percent", "move blind to -15 percent"),
-    ("move blinds to negative four percent", "move blind to -4 percent"),
-    ("move blinds to negative forty-nine percent", "move blind to -49 percent"),
-    ("move blinds to negative one hundred percent", "move blind to -100 percent"),
+    ("move blinds to minus twelve percent", "move blind -12 percent"),
+    ("move blinds to minus sixty three percent", "move blind -63 percent"),
+    ("move blinds to minus fifteen percent", "move blind -15 percent"),
+    ("move blinds to negative four percent", "move blind -4 percent"),
+    ("move blinds to negative forty-nine percent", "move blind -49 percent"),
+    ("move blinds to negative one hundred percent", "move blind -100 percent"),
     # Negative Decimals
-    ("move blinds to minus eighteen point nineteen percent", "move blind to -18.19 percent"),
-    ("move blinds to minus twenty seven point one three six percent", "move blind to -27.136 percent"),
-    ("move blinds to minus seven point two percent", "move blind to -7.2 percent"),
-    ("move blinds to negative four point five eight percent", "move blind to -4.58 percent"),
-    ("move blinds to negative three three point seventy-seven percent", "move blind to -33.77 percent"),
-    ("move blinds to negative eleven point three five one percent", "move blind to -11.351 percent"),
+    ("move blinds to minus eighteen point nineteen percent", "move blind -18.19 percent"),
+    ("move blinds to minus twenty seven point one three six percent", "move blind -27.136 percent"),
+    ("move blinds to minus seven point two percent", "move blind -7.2 percent"),
+    ("move blinds to negative four point five eight percent", "move blind -4.58 percent"),
+    ("move blinds to negative three three point seventy-seven percent", "move blind -33.77 percent"),
+    ("move blinds to negative eleven point three five one percent", "move blind -11.351 percent"),
 ])
 def test_parse_voice(input_line, expected):
     result = VoiceParser.parse(input_line)
