ddaf0d82b2f2343ba1b6befa2eae10001c699592
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-03-27 12:45:28 -0600

    Implement voice parser

---
+++ rpi_voice_control/voice_parser.py
@@ -0,0 +1,21 @@
+from text2digits.text2digits import Text2Digits
+import re
+
+class VoiceParser():
+    # Class variables
+    TEXT2D = Text2Digits()
+    DECIMAL_PATTERN = re.compile(r"(?:(\d+)\s*point\s*(\d+))")
+    NEGATIVE_PATTERN = re.compile(r"(?:(?:minus|negative)\s*(\d+(?:\.\d+)*))")
+
+    @classmethod
+    def parse(cls, line):
+        # Replace substrings with number as word to digits
+        # Ex. "one hundred" -> "100"
+        result = cls.TEXT2D.convert(line)
+        # Replace substrings with integers around word "point" with decimal representation
+        # Ex. "five point 7" -> "5.7" 
+        result = cls.DECIMAL_PATTERN.sub(r"\g<1>.\g<2>", result)
+        # Replace substrings with "minus"/"negative" preceding integer/decimal with -integer/decimal
+        # Ex. "minus 13.159" -> "-13.159"
+        result = cls.NEGATIVE_PATTERN.sub(r"-\g<1>", result)
+        return result
