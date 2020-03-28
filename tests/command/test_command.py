34fc9a8da12d36aa44ef25374342333ae508651a
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-03-27 18:18:37 -0600

    Add unit tests for CommandFactory

---
+++ tests/command/test_command.py
@@ -0,0 +1,20 @@
+import pytest
+from rpi_voice_control.command.command import CommandFactory
+from rpi_voice_control.command.position import PositionCommand
+
+@pytest.mark.parametrize("input_text", [
+    (""),
+    ("las;lkfjkdsajflkjsalkfjalfkjf"),
+])
+def test_command_factory_invalid_input(input_text):
+    command = CommandFactory.build(input_text)
+    assert command is None
+
+@pytest.mark.parametrize("input_text,expected", [
+    ("move blind 15.15% 90m", PositionCommand(15.15, 90)),
+    ("move blind -18.4531% 16h", PositionCommand(-18.4531, 16 * 60)),
+])
+def test_command_factory_invalid_input(input_text, expected):
+    command = CommandFactory.build(input_text)
+    assert isinstance(command, expected.__class__)
+    assert command == expected
