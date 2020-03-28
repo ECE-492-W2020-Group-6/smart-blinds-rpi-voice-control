880eefb793daaf208ba748603901bb444edd97ae
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-03-27 18:02:38 -0600

    Add command interface

---
+++ rpi_voice_control/command/command.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta, abstractmethod, abstractclassmethod
+
+class Command(metaclass=ABCMeta):
+
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError
+
+    @abstractclassmethod
+    def build(cls, text):
+        raise NotImplementedError 
+
+class CommandFactory():
+
+    @staticmethod
+    def build(text):
+        for subclass in Command.__subclasses__():
+            command = subclass.build(text) 
+            if command is not None:
+                return command
+
+        return None
+            
